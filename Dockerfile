3d7263079738bb25438b32bf3f32e775cb185ca4
elango-ela <elango@example.com>
2026-01-11 08:46:01 +0530

    Track raw dataset with DVC

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM python:3.10-slim
 
 WORKDIR /app
-COPY . /app
+COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
+
+# Pull data + models from DVC remote
+RUN dvc pull
 
 CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Breast Cancer Prediction API")
+model = None
+scaler = None
 
-scaler = joblib.load("models/scaler.pkl")
-model = joblib.load("models/model.pkl")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global model, scaler
+    scaler = joblib.load("models/scaler.pkl")
+    model = joblib.load("models/model.pkl")
+    yield   # API runs here
+    # (optional cleanup when shutting down)
+
+app = FastAPI(title="Breast Cancer Prediction API", lifespan=lifespan)
 
 class Features(BaseModel):
     features: list[float]
@@ -18,7 +27,4 @@ def home():
 def predict(data: Features):
     X = scaler.transform([data.features])
     pred = model.predict(X)[0]
-
-    return {
-        "prediction": "Malignant" if int(pred) == 1 else "Benign"
-    }
+    return {"prediction": "Malignant" if int(pred) == 1 else "Benign"}
